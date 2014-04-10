ca74d25b349f9fe0308533e0d1d680bf9f6e2e59
Oliver Johnson <oliverdejohnson@gmail.com>
2014-04-10 15:46:50 +0100

    added all files in folder

---
+++ MvcBootStrap/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcBootStrap.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Index Page.";
+
+            return View();
+        }
+
+
+        public ActionResult DashBoard()
+        {
+            ViewBag.Message = "App Dash Board.";
+
+            return View();
+        }
+
+        public ActionResult Charts()
+        {
+            ViewBag.Message = "App Charts.";
+
+            return View();
+        }
+
+        public ActionResult Table()
+        {
+            ViewBag.Message = "App Charts.";
+
+            return View();
+        }
+    }
+}
